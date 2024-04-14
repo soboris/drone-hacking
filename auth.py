c8211f9c85b6d242694a3a5d5475658c82544f79
soboris <43875638+soboris@users.noreply.github.com>
2024-04-14 17:34:34 +0800

    BlackHat USA 2024 Training

--- auth.py
+++ auth.py
@@ -4,8 +4,10 @@ import crypto
 import hmac
 
 CLA = b'\xff'
-INS = {'req':b'\x01','res':b'\x02'}
+INS = {'req':b'\x01','res':b'\x02','sync':b'\x03'}
 SHA = 256
+
+counter_size = 4
 
 class Token(object):
     def __init__(self, size=16):
@@ -41,9 +43,25 @@ def verifyToken(req, res):
             return True
     return False
 
+def syncCounter():
+    counter = uos.urandom(counter_size)
+    key = crypto.getKey()
+    iv = crypto.getIV()
+    seq = crypto.encrypt(counter, key=key, iv=iv)
+    return int.from_bytes(counter, 'big'), CLA + INS['sync'] + iv + seq
+
+def retCounter(sync):
+    if bytes([sync[0]]) == CLA and bytes([sync[1]]) == INS['sync']:
+        seq = sync[2+crypto.BLOCK:]
+        key = crypto.getKey()
+        iv = sync[2:2+crypto.BLOCK]
+        counter = crypto.decrypt(seq, key, iv).split(b'\x00', 1)[0]
+        return int.from_bytes(counter, 'big')
+    return None
+
 def hexDigest(msg, counter=None):
     if counter is not None:
-        msg = msg + counter.to_bytes(4, 'big')
+        msg = msg + counter.to_bytes(counter_size, 'big')
     digest = hmac.new(crypto.getKey(), msg=msg, digestmod='sha256').hexdigest()
     return digest
 

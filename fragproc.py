54e04f1394881eabb871b54951539270e76242b9
soboris <43875638+soboris@users.noreply.github.com>
2024-04-24 03:13:48 +0800

    BlackHat USA 2024 Training

---
+++ fragproc.py
@@ -0,0 +1,41 @@
+import math
+import binascii
+
+LEN_P = 16
+
+class Packet(object):
+    def __init__(self, packet):
+        self.cla = bytes([packet[0]])
+        self.ins = bytes([packet[1]])
+        self.data = packet[2:]
+
+    def frag(self):
+        fragments = []
+        count = math.ceil(len(self.data)/LEN_P)
+        for i in range(0, count):
+            header = self.cla + self.ins + bytes([count]) + bytes([i])
+            fragments.append(header + self.data[i*LEN_P:(i+1)*LEN_P])
+        return fragments
+
+    @classmethod
+    def reassembly(cls, fragments):
+        data = b''
+        cla = None
+        ins = None
+        count = None
+        fragments.sort()
+        last = fragments[len(fragments)-1]
+        if last[2] - last[3] != 1:
+            return None
+        for fragment in fragments:
+            if cla and ins:
+                if cla != fragment[0] or ins != fragment[1]:
+                    raise Exception("ERROR")
+            cla = fragment[0]
+            ins = fragment[1]
+            if count:
+                if count != fragment[2]:
+                    break
+            count = fragment[2]
+            data += fragment[4:]
+        return cls(bytes([cla]) + bytes([ins]) + data)

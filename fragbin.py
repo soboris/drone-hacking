370bc10410c1177f39fc1cf88316ec77668eb698
soboris <43875638+soboris@users.noreply.github.com>
2024-06-02 16:52:51 +0800

    BlackHat USA 2024 Training

--- fragbin.py
+++ fragbin.py
@@ -1,20 +1,20 @@
 from fragproc import Packet
 
 class Bin(object):
-	def __init__(self):
-		self.bin = []
+    def __init__(self):
+        self.bin = []
 
-	def putFragment(self, frag):
-		self.bin.append(frag)
+    def putFragment(self, frag):
+        self.bin.append(frag)
 
-	def getFragments(self):
-		return self.bin
+    def getFragments(self):
+        return self.bin
 
-	def clearBin(self):
-		self.bin.clear()
+    def clearBin(self):
+        self.bin.clear()
 
-	def checkPacket(self):
-		packet = Packet.reassembly(self.getFragments())
-		if packet:
-			self.clearBin()
-		return packet.bytes()
+    def checkPacket(self):
+        packet = Packet.reassembly(self.getFragments())
+        if packet:
+            self.clearBin()
+        return packet.bytes()

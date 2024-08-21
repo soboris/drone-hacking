5f9c759246948ab19a3648e718d266c7fc3835fd
soboris <43875638+soboris@users.noreply.github.com>
2024-08-21 09:29:52 +0800

    BlackHat USA 2024 Training

--- upload.py
+++ upload.py
@@ -2,6 +2,15 @@ import sys
 import os
 import subprocess
 
+# Update the path of pyDrone/pyController directory
+directory_paths = ['/dev/cu.usbmodem1234561', '/dev/cu.usbmodem2']
+
+# Update the full path of pyDrone directory
+targetDir_pyDrone = '/Users/cwit/Desktop/drone-hacking/drone-hacking-main/pyDrone'
+
+# Update the full path of pyController directory
+targetDir_pyController = '/Users/cwit/Desktop/drone-hacking/drone-hacking-main/pyController'
+
 def upload_files(directory_path, target_directory):
     
     files = os.listdir(target_directory)
@@ -19,53 +28,69 @@ def upload_files(directory_path, target_directory):
                 exception_occurred = True
                 print("Error: Press rst key to reset your device.")
                 break
-        #else:
-            #print("Skipping directory:", file_path)
     if not exception_occurred:
         print("Done")
             
-def is_pycontroller(directory_path):
-    command = f"ampy --port {directory_path} -b 115200 get main.py | grep ble_simple_central"
+def is_pycontroller(osVer, directory_path):
+    if osVer == "Windows":
+        command = f"ampy --port {directory_path} -b 115200 ls | findstr -i ble_simple_central"
+    elif osVer == "Linux":
+        command = f"ampy --port {directory_path} -b 115200 ls | grep ble_simple_central"
+    
     try:
-        result = subprocess.run(command, shell=True, capture_output=True, text=True, timeout=10)
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True, timeout=10, universal_newlines=True)
         if "ble_simple_central" in result.stdout:
             return True
-        else:
-            return False
-    except subprocess.TimeoutExpired:
-        return False
 
-def is_pydrone(directory_path):
-    command = f"ampy --port {directory_path} -b 115200 get main.py | grep ble_simple_peripheral"
+    except subprocess.CalledProcessError as exc:
+        err_msg = "Status : FAIL" + str(exc.returncode) + str(exc.output)
+        raise Exception(err_msg)
+        
+def is_pydrone(osVer, directory_path):
+    if osVer == "Windows":
+        command = f"ampy --port {directory_path} -b 115200 ls | findstr -i ble_simple_peripheral"
+    elif osVer == "Linux":
+        command = f"ampy --port {directory_path} -b 115200 ls | grep ble_simple_peripheral"
     try:
-        result = subprocess.run(command, shell=True, capture_output=True, text=True, timeout=10)
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True, timeout=10, universal_newlines=True)
         if "ble_simple_peripheral" in result.stdout:
             return True
-        else:
-            return False
-    except subprocess.TimeoutExpired:
-        return False
 
-# Update the path of pyDrone/pyController directory
-directory_path = '/dev/cu.usbmodem1234561'
+    except subprocess.CalledProcessError as exc:
+        err_msg = "Status : FAIL" + str(exc.returncode) + str(exc.output)
+        raise Exception(err_msg)
 
-if os.path.exists(directory_path):
-    # Update the full path of pyDrone/pyController directory
-    target_directory = '/Users/cwit/Desktop/drone-hacking/drone-hacking-main/pyDrone' 
-    
-    if "pyDrone" in target_directory:
-        #print("pyDrone")
-        if is_pydrone(directory_path):
-            upload_files(directory_path, target_directory)
-        else:
-            print("This is not pyDrone.")
-    elif "pyController" in target_directory:
-        print("pyController")
-        if is_pycontroller(directory_path):
-            upload_files(directory_path, target_directory)
-        else:
-            print("This is not pyController.")
-    else:
-        print("The specified directory is not the expected type.")
+command = "dir"
+try:
+    chkos = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True, timeout=3, universal_newlines=True)
+except subprocess.CalledProcessError as exc:
+    osVer = "Linux"
 else:
-    print("Directory does not exist:", directory_path)
+    print("Output: \n{}\n".format(chkos))
+    osVer = "Windows"
+
+for path in directory_paths:
+    if os.path.exists(path):
+        try:
+            if is_pydrone(osVer, path):
+                print("pyDrone")
+                
+                if "pyDrone" in targetDir_pyDrone:
+                    upload_files(path, targetDir_pyDrone)
+                else:
+                    print("This is not pyDrone directory.")
+
+            elif is_pycontroller(osVer, path):
+                print("pyController")
+
+                if "pyController" in targetDir_pyController:
+                    upload_files(path, targetDir_pyController)
+                else:
+                    print("This is not pyController directory.")
+            else:
+                print("The specified directory is not the expected type.")
+        
+        except Exception as e:
+            print(e)
+    else:
+        print("Directory does not exist:", path)
